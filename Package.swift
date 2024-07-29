717032eb431d63753a72ce74913549237f1778aa
陈 <2153953+enefry@users.noreply.github.com>
2024-07-29 11:11:05 +0800

    add 3.6.0 url:https://github.com/MobileVLCKit-SPM/MobileVLCKit-SPM/releases/download/FileStorage/MobileVLCKit-3.6.0.xcframework.zip sha256:b685f1d7088449d8def526cde560b26a95ab781e0498e5dce43f51630da74ce0

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MobileVLCKit",
-            url:"https://github.com/MobileVLCKit-SPM/MobileVLCKit-SPM/releases/download/FileStorage/MobileVLCKit-3.5.1.xcframework.zip",
-            checksum:"7cc2b1b436468a6285358291a7896ab90d9710461fd8891c4aea70259763ca8b"
+            url:"https://github.com/MobileVLCKit-SPM/MobileVLCKit-SPM/releases/download/FileStorage/MobileVLCKit-3.6.0.xcframework.zip",
+            checksum:"b685f1d7088449d8def526cde560b26a95ab781e0498e5dce43f51630da74ce0"
         ),
         .target(
             name: "MobileVLCKitSampleViewController",
